c1d09803710c9fe5ee055bd5cd8281af88d74900
FastTurtle <ins1500@naver.com>
2024-01-02 18:41:14 +0900

    checkCollision 완성

--- pacman.js
+++ pacman.js
@@ -1,80 +1,95 @@
 class Pacman {
-    constructor(x, y, width, height, speed) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
-        this.direction = DIRECTION.RIGHT;
+  constructor(x, y, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.direction = DIRECTION.RIGHT;
+  }
+
+  moveProcess() {
+    this.changeDirectionIfPossible();
+    this.moveForward();
+    if (this.checkCollision()) {
+      this.moveBackward();
+    }
+  }
+
+  eat() {}
+
+  moveBackward() {
+    switch (this.direction) {
+      case DIRECTION.RIGHT:
+        this.x -= this.speed;
+        break;
+      case DIRECTION.UP:
+        this.y += this.speed;
+        break;
+      case DIRECTION.LEFT:
+        this.x += this.speed;
+        break;
+      case DIRECTION.BOTTOM:
+        this.y -= this.speed;
+        break;
+    }
+  }
+
+  moveForward() {
+    switch (this.direction) {
+      case DIRECTION.RIGHT:
+        this.x += this.speed;
+        break;
+      case DIRECTION.UP:
+        this.y -= this.speed;
+        break;
+      case DIRECTION.LEFT:
+        this.x -= this.speed;
+        break;
+      case DIRECTION.BOTTOM:
+        this.y += this.speed;
+        break;
+    }
+  }
+
+  checkCollision() {
+    let isCollided = false;
+    if (
+      map[this.getMapY()][this.getMapX()] == 1 ||
+      map[this.getMapYRightSide()][this.getMapX()] == 1 ||
+      map[this.getMapY()][this.getMapXRightSide()] == 1 ||
+      map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+    ) {
+      return true;
     }
 
-    moveProcess() {
-        this.changeDirectionIfPossible();
-        this.moveForward();
-        if (this.checkCollision()) {
-            this.moveBackward();
-        }
-    }
+    return false;
+  }
 
-    eat() {}
+  checkGhostCollision() {}
 
-    moveBackward() {
-        switch (this.direction) {
-            case DIRECTION.RIGHT:
-                this.x -= this.speed;
-                break;
-            case DIRECTION.UP:
-                this.y += this.speed;
-                break;
-            case DIRECTION.LEFT:
-                this.x += this.speed;
-                break;
-            case DIRECTION.BOTTOM:
-                this.y -= this.speed;
-                break;
-        }
-    }
+  changeDirectionIfPossible() {}
 
-    moveForward() {
-        switch (this.direction) {
-            case DIRECTION.RIGHT:
-                this.x += this.speed;
-                break;
-            case DIRECTION.UP:
-                this.y -= this.speed;
-                break;
-            case DIRECTION.LEFT:
-                this.x -= this.speed;
-                break;
-            case DIRECTION.BOTTOM:
-                this.y += this.speed;
-                break;
-        }
-    }
+  changeAnimation() {}
 
-    checkCollision() {}
+  draw() {}
 
-    checkGhostCollision() {}
+  // 현재 x좌표
+  getMapX() {
+    return parseInt(this.x / blockSize);
+  }
 
-    changeDirectionIfPossible() {}
+  // 현재 y좌표
+  getMapY() {
+    return parseInt(this.y / blockSize);
+  }
 
-    changeAnimation() {}
+  //
+  getMapXRightSide() {
+    return parseInt((this.x + 0.9999 * blockSize) / blockSize);
+  }
 
-    draw() {}
-
-    getMapX() {
-        return parseInt(this.x / blockSize);
-    }
-
-    getMapY() {
-        return parseInt(this.y / blockSize);
-    }
-
-    getMapXRightSide() {
-        return parseInt((this.x + 0.9999 * blockSize) / blockSize);
-    }
-
-    getMapYRightSide() {
-        return parseInt((this.y + 0.9999 * blockSize) / blockSize);
-    }
+  getMapYRightSide() {
+    return parseInt((this.y + 0.9999 * blockSize) / blockSize);
+  }
 }
